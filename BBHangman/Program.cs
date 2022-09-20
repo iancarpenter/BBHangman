d63f415956b63e1ab99dc40b9acd0d59eebf3dde
iancarpenter <ianhcarpenter@gmail.com>
2022-09-20 20:01:08 +0100

    removed comment code, output of each turn changed, max guesses now 6, check for min list index

--- BBHangman/Program.cs
+++ BBHangman/Program.cs
@@ -14,8 +14,7 @@ namespace BBHangman
 
             Console.WriteLine($"The word is {game.TheWord}");
 
-            // play continues until the player has run out of lives
-            //while (player.NumberOfIncorrectGuesses() > 0)
+            // play continues until the player has run out of lives            
             while (player.NumberOfIncorrectGuesses() < player.maxNumberOfGuesses)
             {                                
                 string? playersGuess = "";
@@ -54,13 +53,12 @@ namespace BBHangman
 
                 if (!game.TheWord.Contains(playersGuess))
                 {
-                    player.IncorrectGuess();
-
-                    Console.WriteLine($"{game.ShowHangman(player.NumberOfIncorrectGuesses())}");                    
-                    
+                    player.IncorrectGuess();                    
                 }
 
-                Console.WriteLine($"Word: {board} | Remaining: {player.NumberOfIncorrectGuesses()}  | Incorrect: | Guess: {playersGuess} ");
+                Console.WriteLine($"{game.ShowHangman(player.NumberOfIncorrectGuesses())}");
+
+                Console.WriteLine($"Word: {board}");
                                
                 if (charactersLeftToGuess == 0)
                 {
@@ -103,87 +101,93 @@ namespace BBHangman
     }
 }
 
-    public class Player
+public class Player
+{
+    private List<string> _guesses = new List<string>();
+    public List<string> Guesses { get => _guesses; }
+    public string name { get; set; }
+
+    public int maxNumberOfGuesses { get; }
+
+    private int _incorrectGuess;
+    public int NumberOfIncorrectGuesses() => _incorrectGuess;
+
+    public Player(string name, int maxNumberOfGuesses = 6)
     {
-        private List<string> _guesses = new List<string>();
-        public List<string> Guesses { get => _guesses; }
-        public string name { get; set; }
+        this.name = name;
 
-        public int maxNumberOfGuesses { get; }
-
-        private int _incorrectGuess;
-        public int NumberOfIncorrectGuesses() => _incorrectGuess;
-
-        public Player(string name, int maxNumberOfGuesses = 7)
-        {
-            this.name = name;
-
-            this._incorrectGuess = 0;
+        this._incorrectGuess = 0;
             
-            this.maxNumberOfGuesses = maxNumberOfGuesses;
-        }
-
-        public bool LetterAlreadyUsed(string letter)
-        {
-            if (_guesses.Contains(letter) )
-            {
-                return true;
-            }
-            else
-            {
-                _guesses.Add(letter);
-                return false;
-            }
-        }
-
-        public void IncorrectGuess()
-        {
-            _incorrectGuess++;           
-        }
-        
+        this.maxNumberOfGuesses = maxNumberOfGuesses;
     }
 
-    public class Game
+    public bool LetterAlreadyUsed(string letter)
     {
-        List<string> availableWords = new List<string>() { "beantobar", "coconut", "tufftoffee", "peppermint", "orangecreme", "caramel" };
-
-        public string TheWord { get; }
-
-        public Game()
+        if (_guesses.Contains(letter) )
         {
-            
-            Random r = new Random();
-
-            int rInt = r.Next(0, 5);
-            
-            this.TheWord = availableWords[rInt];
+            return true;
         }
-
-        public void Results(bool playerWon, string playersName)
+        else
         {
-           if (playerWon)
-           {
-              Console.WriteLine($"Congratulations {playersName}, you have won");
-           }
-           else
-           {
-              Console.WriteLine($"Sorry {playersName} you have lost");
-           }
+            _guesses.Add(letter);
+            return false;
         }
-
-        private List<string> hangManGraphic = new List<string>() { "+---+\r\n  |   |\r\n      |\r\n      |\r\n      |\r\n      |\r\n=========",
-                                                                   "+---+\r\n  |   |\r\n  O   |\r\n      |\r\n      |\r\n      |\r\n=========",
-                                                                   "+---+\r\n  |   |\r\n  O   |\r\n  |   |\r\n      |\r\n      |\r\n=========",
-                                                                   "+---+\r\n  |   |\r\n  O   |\r\n /|   |\r\n      |\r\n      |\r\n=========",
-                                                                   "+---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n      |\r\n      |\r\n=========",
-                                                                   "+---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n /    |\r\n      |\r\n=========",
-                                                                   "+---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n / \\  |\r\n      |\r\n=========",
-        };
-        
-        public string ShowHangman (int pictureNumber)
-        {
-           //var descendingOrder = hangManGraphic.OrderByDescending(i => pictureNumber);
-           return hangManGraphic[--pictureNumber];
-        }
-
     }
+
+    public void IncorrectGuess()
+    {
+        _incorrectGuess++;           
+    }
+        
+}
+
+public class Game
+{
+    List<string> availableWords = new List<string>() { "beantobar", "coconut", "tufftoffee", "peppermint", "orangecreme", "caramel" };
+    
+    public string TheWord { get; }
+
+    public Game()
+    {
+            
+        Random r = new Random();
+
+        int rInt = r.Next(0, 6);
+            
+        this.TheWord = availableWords[rInt];
+    }
+
+    public void Results(bool playerWon, string playersName)
+    {
+        if (playerWon)
+        {
+            Console.WriteLine($"Congratulations {playersName}, you have won");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry {playersName} you have lost");
+        }
+    }
+
+    private List<string> hangManGraphic = new List<string>() { "+---+\r\n  |   |\r\n      |\r\n      |\r\n      |\r\n      |\r\n=========",
+                                                               "+---+\r\n  |   |\r\n  O   |\r\n      |\r\n      |\r\n      |\r\n=========",
+                                                               "+---+\r\n  |   |\r\n  O   |\r\n  |   |\r\n      |\r\n      |\r\n=========",
+                                                               "+---+\r\n  |   |\r\n  O   |\r\n /|   |\r\n      |\r\n      |\r\n=========",
+                                                               "+---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n      |\r\n      |\r\n=========",
+                                                               "+---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n /    |\r\n      |\r\n=========",
+                                                               "+---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n / \\  |\r\n      |\r\n=========",
+    };
+        
+    public string ShowHangman (int pictureNumber)
+    {
+        int i = pictureNumber;
+
+        if (i < 0)
+        {
+            i = 0;
+        }
+
+        return hangManGraphic[i];
+    }
+
+}
